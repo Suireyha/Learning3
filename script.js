e177de51435f19a13cfbdabca39984c92015174e
Suireyha <Marvink2296@gmail.com>
2022-09-21 02:54:59 +1000

    Got the palindrome function, just need to display results to user

--- script.js
+++ script.js
@@ -1,103 +1,22 @@
-let plus = document.querySelector('.plus');
-let minus = document.querySelector('.minus');
-let multiply = document.querySelector('.multiply');
-let divide = document.querySelector('.divide');
-let equals = document.querySelector('.equals');
-let numberButton = document.querySelectorAll('.number');
+let label = document.querySelector('.label');
 let display = document.querySelector('#display');
-let clear = document.querySelector('.clear');
+let submit = document.querySelector('.submit');
 
-var uInput = "";
-var uAdd = false;
-var uMinus = false;
-var uMultiply = false;
-var uDivide = false;
 
-let calculator = {
-    '+': (x, y)=>{
-        return x+y;
-    },
+function palindrome(str){
 
-    help: (x)=>{
-        return x+5;
+    var temp = [];
+    for (var i = 0; i < str.length; i++){
+        temp[i] = str[str.length - (1 + i)];
     }
-};
-
-plus.addEventListener('click', ()=>{ 
-    uAdd = true;
-    uMinus = false;
-    uMultiply = false;
-    uDivide = false;
-    display.value += '+';
-    display.select
-    display.focus();
-});
-
-minus.addEventListener('click', ()=>{
-    uAdd = false;
-    uMinus = true;
-    uMultiply = false;
-    uDivide = false;
-    display.value += '-';
-    display.select
-    display.focus();
-
-});
-
-multiply.addEventListener('click', ()=>{
-    uAdd = false;
-    uMinus = false;
-    uMultiply = true;
-    uDivide = false;
-    display.value += '*';
-    display.select
-    display.focus();
-
-});
-
-divide.addEventListener('click', ()=>{
-    uAdd = false;
-    uMinus = false;
-    uMultiply = false;
-    uDivide = true;
-    display.value += '/';
-    display.select
-    display.focus();
-
-});
-
-clear.addEventListener('click', ()=>{
-    display.value = "";
-});
-
-equals.addEventListener('click', ()=>{
-    uInput = display.value;
-    display.value = null;
-    
-
-    if(uAdd){
-        ind = uInput.indexOf("+");
-        display.value = ((+uInput.substring(0, ind)) + (+uInput.substring(ind + 1, uInput.length)));
+    var com = temp.join("");
+    console.log(com);
+    if (com == str){
+        console.log("True");
+        return true;
     }
-    else if(uMinus){
-        ind = uInput.indexOf("-");
-        display.value = ((+uInput.substring(0, ind)) - (+uInput.substring(ind + 1, uInput.length)));
-    }
-    else if(uMultiply){
-        ind = uInput.indexOf("*");
-        display.value = ((+uInput.substring(0, ind)) * (+uInput.substring(ind + 1, uInput.length)));
-    }
-    else if(uDivide){
-        ind = uInput.indexOf("/");
-        display.value = ((+uInput.substring(0, ind)) / (+uInput.substring(ind + 1, uInput.length)));
-    }
-});
+    console.log("False");
+    return false;
+}
 
-numberButton.forEach(el =>{
-    el.addEventListener('click', (e)=>{
-        display.value += e.target.innerText
-    });
-});
-
-
-//Add multiple numbers
+submit.addEventListener('click', ()=>{palindrome(display.value)});

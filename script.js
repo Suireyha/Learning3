c7445c364ddfa7380fa689f9196418788f2c66a9
Suireyha <Marvink2296@gmail.com>
2022-09-21 06:22:13 +1000

    Currently in a very buggy state, debugging

--- script.js
+++ script.js
@@ -1,26 +1,145 @@
-let label = document.querySelector('.label');
+let plus = document.querySelector('.plus');
+let minus = document.querySelector('.minus');
+let multiply = document.querySelector('.multiply');
+let divide = document.querySelector('.divide');
+let equals = document.querySelector('.equals');
+let numberButton = document.querySelectorAll('.number');
+let operatorButtons = document.querySelectorAll('.operator');
 let display = document.querySelector('#display');
-let submit = document.querySelector('.submit');
+let clear = document.querySelector('.clear');
+
+var uInput = "";
+var uAdd = false;
+var uMinus = false;
+var uMultiply = false;
+var uDivide = false;
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function fib(int){
+let calculator = {
+    '+': (x, y)=>{
+        return x+y;
+    },
 
-    let sequence = [];
-    
-    for (var i = 0; i < int; i++){
-        if(i < 2){
-            sequence[i] = 1;
-        }
-        else{
-            sequence[i] = (sequence[i - 1] + sequence[i - 2]);
-        }
+    '-': (x, y)=>{
+        return x-y;
+    },
+
+    '*': (x, y)=>{
+        return x*y;
+    },
+
+    '/': (x, y)=>{
+        return x/y;
     }
+};
 
-    display.value = (sequence[sequence.length - 1]);
+function operate(operator, a, b){
+    display.value = (calculator[operator](a, b));
 }
 
+let caleb = [0, 1, 2, 1, 4, 5];
+console.log(caleb.find(el => el == 4 | el == 5));
 
-submit.addEventListener('click', ()=>{fib(display.value)});
+
+clear.addEventListener('click', ()=>{
+    display.value = "";
+});
+
+equals.addEventListener('click', ()=>{
+    var operation;
+    var next;
+    var total;
+    let copy = display.value;
+    uInput = display.value;
+    display.value = null;
+    
+    
+
+    operation = uInput.indexOf("+", "-", "*", "/");
+
+    copy = copy.slice(copy.indexOf(operation));
+
+    next = copy.indexOf("+", "-", "*", "/");
+
+    ind = uInput.indexOf(operation);
+    var operand1 = uInput.substring(0, ind);
+
+    console.log(operation);
+
+    while (operation != -1){
+        
+        console.log(operation);
+
+        console.log(next);
+
+        if (next == -1){
+            var operand2 = uInput.substring(ind + 1, uInput.length);
+        }
+        else{
+            var operand2 = uInput.substring(ind + 1, next);
+        }
+
+        console.log(operand1);
+        console.log(operand2);
+
+        switch(uInput[operation]){
+            case '+':
+                total += operate('+', operand1, operand2);
+                operand1 = total;
+                break;
+            case '-':
+                total += operate('-', operand1, operand2);
+                operand1 = total;
+                break;
+            case '*':
+                total += operate('*', operand1, operand2);
+                operand1 = total;
+                break;
+            case '/':
+                total += operate('/', operand1, operand2);
+                operand1 = total;
+                break;
+            default:
+                console.log("Not an operator");
+        }
+
+        operation = copy.indexOf("+", "-", "*", "/");
+
+        copy = copy.slice(copy.indexOf(operation));
+
+        next = copy.indexOf("+", "-", "*", "/");
+
+    }
+
+    display.value = total;
+});
+
+numberButton.forEach(el =>{
+    el.addEventListener('click', (e)=>{
+        display.value += e.target.innerText
+    });
+});
+
+operatorButtons.forEach(el =>{
+    el.addEventListener('click', (e)=>{
+        switch (e.target.innerText){
+            case '+':
+                display.value += '+';
+                break;
+            case '-':
+                display.value += '-';
+                break;
+            case 'x':
+                display.value += '*';
+                break;
+            case '/':
+                display.value += '/';
+                break;
+        }
+        display.select;
+        display.focus();
+    })
+})

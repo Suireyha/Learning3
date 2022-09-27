2986db2be96a5992d6e2fd9b50d5f542a5ae8938
Suireyha <Marvink2296@gmail.com>
2022-09-28 01:50:58 +1000

    I'm finished with this, it works perfectly except for calculations with negative numbers, or if there's too many operators.
    To my credit, the assignment asked for only 1 operator at a time so

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ let numberButton = document.querySelectorAll('.number');
 let operatorButtons = document.querySelectorAll('.operator');
 let display = document.querySelector('#display');
 let clear = document.querySelector('.clear');
-
 var uInput = "";
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+//Contains definitions for various operations, only accessed via operate()
 let calculator = {
     '+': (x, y)=>{
         return x+y;
@@ -32,31 +32,24 @@ let calculator = {
     }
 };
 
+//The user-friendly calculation function
 function operate(operator, a, b){
     return (calculator[operator](a, b));
 }
 
+//Clear the display when clear button is pressed
 clear.addEventListener('click', ()=>{
     display.value = "";
 });
 
-function getIndex(element){
-    if (element > 0){
-
-    }
-}
-
-let example = "2+3";
-console.log(example.substring(2));
-
+//Might as well be main()
 equals.addEventListener('click', ()=>{
     var operation = -1;
     var next = -1;
     var total;
-    var isOperator;
+    var isOperator = false;
     let copy = display.value;
     uInput = display.value;
-    //display.value = null;
     
     /*Disgusting code that figures out the index of the first operator
     *Iterate through each element, if it's an operator then save its value*/
@@ -76,10 +69,9 @@ equals.addEventListener('click', ()=>{
         return;
     }
 
-    console.log(operation);
-
     //copy is the array after the first operator
     copy = copy.slice(copy.indexOf(operation) + 1);
+    console.log(copy);
 
     //If there's a second operator, find it and save it's value
     for (var i = 0; i < copy.length; i++) {
@@ -91,56 +83,41 @@ equals.addEventListener('click', ()=>{
         } 
     };
 
-
+    /*Initialise the index of the first operator, and decleration of
+     index of second operator */
     let ind = uInput.indexOf(operation);
-    let ind2 = uInput.indexOf(next);
-
+    let ind2;
     var operand1 = uInput.substring(0, ind);
-    console.log(operand1);
+    
 
     while (uInput.indexOf(operation) > -1){
 
-        ind = uInput.indexOf(operation);
-
+        //If there are duplicate operators, don't use the wrong index for 2nd
+        if (operation == next){
+            ind2 = uInput.indexOf(next) + uInput.indexOf(operation) + 1;
+        }
+        else{
+            ind2 = uInput.indexOf(next);
+        }
+        
+        //If there's multiple operators, divide the expression into seperate parts
         if (next == -1){
             var operand2 = uInput.substring(ind + 1);
         }
         else{
-            var operand2 = uInput.substring(ind, ind2);
+            var operand2 = uInput.substring(ind + 1, ind2);
         }
 
+        //Calculate expression
         total =+ operate(operation, (+operand1), (+operand2));
         operand1 = total;
 
-        /*
-        switch(uInput[operation]){
-            case '+':
-                total += operate('+', operand1, operand2);
-                operand1 = total;
-                break;
-            case '-':
-                total += operate('-', operand1, operand2);
-                operand1 = total;
-                break;
-            case '*':
-                total += operate('*', operand1, operand2);
-                operand1 = total;
-                break;
-            case '/':
-                total += operate('/', operand1, operand2);
-                operand1 = total;
-                break;
-            default:
-                console.log("Not an operator");
-        }
-        */
-
+        //Get ready for subsequent expression (if multiple operators)
         copy = copy.slice(copy.indexOf(operation + 1));
-
         operation = next;
+
         next = -1;
 
-        
         for (var i = 0; i < copy.length; i++) {
             if (copy.charAt(i) == "+" | copy.charAt(i) == "-" | 
             copy.charAt(i) == "*" | copy.charAt(i) == "/"){
@@ -150,8 +127,9 @@ equals.addEventListener('click', ()=>{
             } 
         };
 
+        console.log(next);
+        ind = uInput.indexOf(operation, ind + 1);
     }
-
     display.value = total;
 });
 
